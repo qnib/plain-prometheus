d5a4e012160a11b4c34039dfcde0613230b6284e
Christian Kniep <christian@qnib.org>
2017-09-04 16:07:47 +0200

    allow for remote-storage

--- opt/entry/20-prometheus.sh
+++ opt/entry/20-prometheus.sh
@@ -3,3 +3,7 @@
 mkdir -p /data/prometheus
 chown prometheus: /data/prometheus
 cp /opt/qnib/prometheus/prometheus.yml /etc/
+if [[ "X${STORAGE_ADAPTER_HOST}" != "X" ]];then
+  sed -i'' -e "s;#\- url:.*\/write\";- url: \"http://${STORAGE_ADAPTER_HOST}:${STORAGE_ADAPTER_PORT}/write\";" \
+           -e "s;#\- url:.*\/read\";- url: \"http://${STORAGE_ADAPTER_HOST}:${STORAGE_ADAPTER_PORT}/read\";" /etc/prometheus.yml
+fi
