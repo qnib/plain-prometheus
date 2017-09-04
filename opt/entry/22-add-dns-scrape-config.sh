edf43e2fea932fe722233edc7c9ab96d04bf005f
Christian Kniep <christian@qnib.org>
2017-09-04 17:06:09 +0200

    Allow for the scrape interval to be changed

--- opt/entry/22-add-dns-scrape-config.sh
+++ opt/entry/22-add-dns-scrape-config.sh
@@ -7,8 +7,13 @@ for CFG in $(echo ${PROMETHEUS_DNS_SCRAPE_LIST} |sed -e 's/,/ /g');do
   SERVICE=$(echo ${CFG} |/usr/bin/awk -F: '{print $2}')
   PORT=$(echo ${CFG} |/usr/bin/awk -F: '{print $3}')
   PATH=$(echo ${CFG} |/usr/bin/awk -F: '{print $4}')
+  SCRAPE_INTERVAL=$(echo ${CFG} |/usr/bin/awk -F: '{print $5}')
+  if [[ "X${SCRAPE_INTERVAL}" != "X" ]];then
+    SCRAPE_INTERVAL=5s
+  fi
   echo """  - job_name: '${NAME}'
     metrics_path: ${PATH}
+    scrape_interval: ${SCRAPE_INTERVAL}
     scheme: http
     dns_sd_configs:
     - names:
