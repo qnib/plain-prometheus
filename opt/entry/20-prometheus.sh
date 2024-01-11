78364cbe8cc5d8d6226cd1e447c35b4d520fbeec
Christian Kniep <christian@qnib.org>
2024-01-11 12:20:47 +0100

    update version

--- opt/entry/20-prometheus.sh
+++ opt/entry/20-prometheus.sh
@@ -7,3 +7,4 @@ if [[ "X${STORAGE_ADAPTER_HOST}" != "X" ]];then
   sed -i'' -e "s;#\- url:.*\/write\";- url: \"http://${STORAGE_ADAPTER_HOST}:${STORAGE_ADAPTER_PORT}/write\";" \
            -e "s;#\- url:.*\/read\";- url: \"http://${STORAGE_ADAPTER_HOST}:${STORAGE_ADAPTER_PORT}/read\";" /etc/prometheus.yml
 fi
+
